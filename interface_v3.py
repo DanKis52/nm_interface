67b54419baadae1d601d91da62b567949f4c5ba4
mafioznik <kiselev.danil201313@gmail.com>
2021-09-28 20:21:54 +0300

    comments & grid

--- interface_v3.py
+++ interface_v3.py
@@ -1,35 +1,43 @@
 import tkinter as tk
 
+
 class Interface:
     def __init__(self, master):
-        self.master = master
-        self.photo = tk.PhotoImage(file='logo.png')
-        master.iconphoto(False, self.photo)
-        master.title('Задача Коши для ОДУ')
-        master.configure(bg='#ececec')
-        master.minsize(1110, 700)
-        self.x0 = tk.StringVar(master, '1')
-        self.u0 = tk.StringVar(master, '2')
-        self.accuracy = tk.StringVar(master, '0.00000001')
-        self.error = tk.StringVar(master, '0.0001')
-        self.max_step = tk.StringVar(master, '10000000')
-        self.step = tk.StringVar(master, '0.01')
-        self.rb_var = tk.IntVar(master)
-        self.rb_var.set(0)
-        self.cb_var = tk.BooleanVar(master)
-        self.cb_var.set(1)
-        self.create_widgets()
+        self.master = master  # инициализируем основное окно
+        self.photo = tk.PhotoImage(file='logo.png')  # загрузка иконки приложения
+        master.iconphoto(False, self.photo)  # установка иконки
+        master.title('Задача Коши для ОДУ')  # заголовок
+        master.configure(bg='#ececec')  # фон
+        master.minsize(1110, 700)  # минимальный размер окна
+        self.x0 = tk.StringVar(master, '1')  # x0
+        self.u0 = tk.StringVar(master, '2')  # u0
+        self.accuracy = tk.StringVar(master, '0.00000001')  # точность выхода на границу
+        self.error = tk.StringVar(master, '0.0001')  # контроль лок. поргрешности
+        self.max_step = tk.StringVar(master, '10000000')  # максю число шагов
+        self.step = tk.StringVar(master, '0.01')  # начальный шаг
+        self.rb_var = tk.IntVar(master)  # хранит 0 или 1 (выход на границу x или u)
+        self.rb_var.set(0)  # значение по умолчанию
+        self.cb_var = tk.BooleanVar(master)  # хранит True или False (включен ли контроль погр-ти)
+        self.cb_var.set(1)  # значение по умолчанию
+        self.create_widgets()  # создание виджетов
 
     def create_widgets(self):
+        # место для задачи (нужно будет поставить скрин\текст)
         task_l = tk.Label(text='место место место\nдля для для\nзадачи задачи задачи\nура ура ура', bg='#ececec').grid(
             row=0, column=0, columnspan=2, rowspan=4, padx=(10, 0), pady=(10, 0))
+
+        # начальные условия
         cond_l = tk.Label(text='Начальные условия', bg='#ececec').grid(row=4, column=0, columnspan=2)
         cond_xl = tk.Label(text='x0', bg='#ececec').grid(row=5, column=0, padx=(10, 0), sticky='w')
         cond_xe = tk.Entry(highlightbackground='#cbcbcb', textvariable=self.x0).grid(row=5, column=1, padx=(0, 10))
         cond_ul = tk.Label(text='u0', bg='#ececec').grid(row=6, column=0, padx=(10, 0), sticky='w')
         cond_ue = tk.Entry(highlightbackground='#cbcbcb', textvariable=self.u0).grid(row=6, column=1, padx=(0, 10))
+
+        # кнопка Вычислить
         exec_b = tk.Button(text='Вычислить', bg='#ececec', highlightbackground='#ececec', command=self.execute).grid(
             row=8, column=0, columnspan=2, padx=10, sticky='we')
+
+        # параметры програмы
         accuracy_l = tk.Label(text='Точность выхода на границу', bg='#ececec').grid(row=0, column=2, padx=10,
                                                                                     pady=(10, 0), sticky='w')
         rb1 = tk.Radiobutton(text='x', variable=self.rb_var, value=0, bg='#ececec').grid(row=0, column=3, pady=(10, 0),
@@ -51,11 +59,17 @@ class Interface:
         step_l = tk.Label(text='Начальный шаг', bg='#ececec').grid(row=7, column=2, padx=10, sticky='w')
         step_e = tk.Entry(highlightbackground='#cbcbcb', textvariable=self.step).grid(row=8, column=2, columnspan=3,
                                                                                       padx=(10, 0), sticky='we')
-        # reference frame
+
+        # справка (реализовать заполнение в функции execute)
         reference_l = tk.Label(text='Справка', bg='#ececec').grid(row=0, column=5, pady=10, sticky='we')
         reference_t = tk.Text(height=14, highlightbackground='#cbcbcb').grid(row=1, column=5, rowspan=9, padx=(10, 0),
                                                                              sticky='we')
 
+        # таблица
+
+        # график
+
+    #  выполняется при нажатии кнопки "Вычислить"
     def execute(self):
         data = [
             self.x0.get(), self.u0.get(), self.accuracy.get(), self.error.get(), self.max_step.get(), self.step.get(),
